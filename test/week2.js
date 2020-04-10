da36d9f9df4c9839f8d858d500a4df7f76a83144
Andres Dvoinos <ajdvoynos@gmail.com>
2020-04-10 18:28:49 +0200

    MinStack problem

--- test/week2.js
+++ test/week2.js
@@ -1,6 +1,7 @@
 import assert from "assert";
 import middleLinkedList, { ListNode, arrayToList } from "../week2/middleLinkedList.js";
 import backspaceStringCompare from "../week2/backspaceStringCompare.js";
+import minStack from "../week2/minStack.js";
 
 describe('Week2', function(){
   describe('middleLinkedList', function(){
@@ -54,5 +55,30 @@ describe('Week2', function(){
       var actual = backspaceStringCompare(...input);
       assert.equal(actual, expected);
     });
+  });
+
+  describe('minStack', function(){
+    it('should handle all methods', function(){
+      var x = new minStack();
+      x.push(-2);
+      x.push(0);
+      x.push(-3);
+      assert.equal(x.getMin(), -3);
+      x.pop();
+      assert.equal(x.top(), 0);
+      assert.equal(x.getMin(), -2);
+    });
+
+    it('should return 2', function(){
+      var x = new minStack();
+      x.push(2);
+      x.push(0);
+      x.push(3);
+      x.push(0);
+      x.pop();
+      x.pop();
+      x.pop();
+      assert.equal(x.getMin(), 2);
+    })
   })
 });

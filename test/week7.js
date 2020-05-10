d3d92dae04b8ffedfeade5371dee2c31b16a557e
Andres Dvoinos <ajdvoynos@gmail.com>
2020-05-10 18:07:27 +0200

    First attempt 😎

--- test/week7.js
+++ test/week7.js
@@ -1,6 +1,7 @@
 import assert from "assert";
 import checkStraightLine from "../week7/straightLine.js";
 import isPerfectSquare from "../week7/validPerfectSquare.js";
+import findJudge from "../week7/findTownJudge.js";
 
 describe('Week7', function () {
   describe('Straight Line', function () {
@@ -37,4 +38,38 @@ describe('Week7', function () {
       assert.equal(actual, expected);
     });
   });
+  describe('Find town judge', function () {
+    it('Should solve example 1', function () {
+      var input = [2, [[1,2]]];
+      var expected = 2;
+      var actual = findJudge(...input);
+      assert.equal(actual, expected);
+    });
+    it('Should solve example 2', function () {
+      var input = [3, [[1,3],[2,3]]];
+      var expected = 3;
+      var actual = findJudge(...input);
+      assert.equal(actual, expected);
+    });
+    it('Should solve example 3', function () {
+      var input = [3, [[1,3],[2,3],[3,1]]];
+      var expected = -1;
+      var actual = findJudge(...input);
+      assert.equal(actual, expected);
+    });
+    it('Should solve example 4', function () {
+      var input = [3, [[1,2],[2,3]]];
+      var expected = -1;
+      var actual = findJudge(...input);
+      assert.equal(actual, expected);
+    });
+    it('Should solve example 5', function () {
+      var input = [4, [[1,3],[1,4],[2,3],[2,4],[4,3]]];
+      var expected = 3;
+      var actual = findJudge(...input);
+      assert.equal(actual, expected);
+    });
+    
+  });
+
 });

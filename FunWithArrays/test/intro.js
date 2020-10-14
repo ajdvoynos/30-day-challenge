37d49bab1dbcdc9e4ad5dbcafe502e465f346909
Andrei Dvoynos <ajdvoynos@gmail.com>
2020-10-14 16:58:26 +0200

    Sorted squares ez pz

--- FunWithArrays/test/intro.js
+++ FunWithArrays/test/intro.js
@@ -1,6 +1,7 @@
 import assert from "assert";
 import findNumbers from "../intro/evenNumberOfDigits.js";
 import findMaxConsecutiveOnes from "../intro/maxConsecutiveOnes.js";
+import sortedSquares from "../intro/squaresSortedArray.js";
 
 describe('IntroArrays', () => {
     describe('MaxConsecutiveOnes', () => {
@@ -21,4 +22,15 @@ describe('IntroArrays', () => {
             assert.equal(actual, expected);
         });
     });
+    
+    describe('SortedSquares', () => {
+        it('Should solve example 1', () => {
+            var input = [-4,-1,0,3,10];
+            var expected = [0,1,9,16,100];
+            var actual = sortedSquares(input);
+
+            assert.deepStrictEqual(actual, expected);
+        });
+    });
+
 });

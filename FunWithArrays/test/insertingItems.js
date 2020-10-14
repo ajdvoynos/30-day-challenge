f62400cfca4f7aa3ed3dc633da360e020147c841
Andrei Dvoynos <ajdvoynos@gmail.com>
2020-10-14 20:03:25 +0200

    99.94% 😎

--- FunWithArrays/test/insertingItems.js
+++ FunWithArrays/test/insertingItems.js
@@ -1,20 +1,51 @@
 import assert from "assert";
 import duplicateZeros from "../inserting items/duplicateZeroes.js";
+import merge from "../inserting items/mergeSorted.js";
 
 describe('InsertingItems', () => {
     describe('Duplicate Zeroes', () => {
         it('Should solve example 1', () => {
             var input = [1,0,2,3,0,4,5,0];
             var expected = [1,0,0,2,3,0,0,4];
-            //duplicateZeroes should modify the array...🙄
+            //method should modify the array...🙄
             duplicateZeros(input)
             assert.deepStrictEqual(input, expected);
         });
         it('Should not modify array', () => {
             var input = [1,2,3];
             var expected = [1,2,3];
+            //method should modify the array...🙄
             duplicateZeros(input)
             assert.deepStrictEqual(input, expected);
         });
+    });
+    describe('Merge Sorted', () => {
+        it('Should solve example 1', () => {
+            var input = [[1,2,3,0,0,0], 3, [2,5,6], 3];
+            //method should modify the array...🙄
+            var expected = [1,2,2,3,5,6];
+            merge(...input);
+
+            assert.deepStrictEqual(input[0], expected);
+        });
+        it('Should solve example 2', () => {
+            var input = [[4,0,0,0,0,0], 1, [1,2,3,5,6], 5];
+            //method should modify the array...🙄
+            var expected = [1,2,3,4,5,6];
+            merge(...input);
+
+            assert.deepStrictEqual(input[0], expected);
+        });
+        it('Should solve example andrei', () => {
+            var input = [[4,0,0,0], 1, [1,2,3], 3];
+            //method should modify the array...🙄
+            var expected = [1,2,3,4];
+            merge(...input);
+
+            assert.deepStrictEqual(input[0], expected);
+        });
+
+
+
     });
 });

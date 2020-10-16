00eb70f2779495a77dbf026d0a4e17461c7dd1a0
Andrei Dvoynos <ajdvoynos@gmail.com>
2020-10-16 15:46:14 +0200

    90.91%

--- FunWithArrays/test/searching.js
+++ FunWithArrays/test/searching.js
@@ -1,5 +1,6 @@
 import assert from "assert";
 import checkIfExist from "../searching/checkIfExists.js";
+import validMountainArray from "../searching/validmountain.js";
 
 describe('Searching Items', () => {
     describe('Check if double exists', () => {
@@ -31,6 +32,50 @@ describe('Searching Items', () => {
             var input = [3,0,1,7,11];
             var expected = false;
             var actual = checkIfExist(input);
+            assert.deepStrictEqual(actual, expected);
+        });
+    });
+    describe('Valid mountain', () => {
+        it('Should solve example 1', () => {
+            var input = [2,1];
+            var expected = false;
+            var actual = validMountainArray(input);
+            assert.deepStrictEqual(actual, expected);
+        });
+        it('Should solve example 1', () => {
+            var input = [2,1];
+            var expected = false;
+            var actual = validMountainArray(input);
+            assert.deepStrictEqual(actual, expected);
+        });
+        it('Should solve example 2', () => {
+            var input = [3,5,5];
+            var expected = false;
+            var actual = validMountainArray(input);
+            assert.deepStrictEqual(actual, expected);
+        });
+        it('Should solve example 3', () => {
+            var input = [0,3,2,1];
+            var expected = true;
+            var actual = validMountainArray(input);
+            assert.deepStrictEqual(actual, expected);
+        });
+        it('Should return false for 1 element array', () => {
+            var input = [1];
+            var expected = false;
+            var actual = validMountainArray(input);
+            assert.deepStrictEqual(actual, expected);
+        });
+        it('Should return false for only going up array', () => {
+            var input = [1,2,3];
+            var expected = false;
+            var actual = validMountainArray(input);
+            assert.deepStrictEqual(actual, expected);
+        });
+        it('Should return false for 2 element array', () => {
+            var input = [1,2];
+            var expected = false;
+            var actual = validMountainArray(input);
             assert.deepStrictEqual(actual, expected);
         });
     });
